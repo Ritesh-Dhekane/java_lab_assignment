c3475cfa90251bd8a5c540d87dd298d331828366
Ritesh Dhekane <ritesh.dhekane@outlook.com>
2026-03-16 20:56:52 +0530

    Made mobile friendly

--- assign4/ExceptionDifferenceDemo.java
+++ assign4/ExceptionDifferenceDemo.java
@@ -6,7 +6,7 @@ public class ExceptionDifferenceDemo {
 
     public static void main(String args[]) {
 
-        System.out.println("devName: Ritesh Dhekane");
+        System.out.println("devName: Aakanksha Mane");
         System.out.println("Q20: Checked vs Unchecked Exception");
 
         try {
